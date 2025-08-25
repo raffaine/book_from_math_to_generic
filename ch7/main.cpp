8109c43ab4b2e88fc89f8895b81693a009714bf1
Julio Raffaine <julio.loxas@gmail.com>
2025-08-25 11:22:00 -0700

    Initial commit

---
+++ ch7/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+
+using namespace std::chrono;
+
+
+int main(int argc, char** argv) {
+    auto duration = 1ms;
+
+    std::cout << "Hello" << std::endl;
+
+    return 0;
+}
